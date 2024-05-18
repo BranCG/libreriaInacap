4bedb9bbe88120afa4bb8087c908498ea2cb3ca2
Carlos Campaña <carlos.campana.d@gmail.com>
2024-05-17 21:13:09 -0400

    se agra obtener libro

--- CRUD/curdLibro.py
+++ CRUD/curdLibro.py
@@ -1,5 +1,6 @@
 from CONEX.conex import conex
 from datetime import datetime
+from CLASES.LIBRO import Libro
 
 def insertar_libro(libro):
     conn = conex()
@@ -10,3 +11,13 @@ def insertar_libro(libro):
     conn.commit()
     libro.codigo_libro = cursor.lastrowid
     conn.close()
+
+def obtener_libro(codigo_libro):
+    conn = conex()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM libro WHERE codigo_libro = %s', (codigo_libro,))
+    row = cursor.fetchone()
+    conn.close()
+    if row:
+        return Libro(codigo_libro=row[0], titulo=row[1], autor=row[2], anio_publicacion=row[3])
+    return None
