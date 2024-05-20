ddc8951f84cf7ce598c46b98a12db9e5e023b038
Brandon Castro <124015960+BranCG@users.noreply.github.com>
2024-05-19 22:58:50 -0400

    AA

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# usuario con md5 para login.
+# main.py
 from MENU.menu import Menu
-from CRUD.encripteitor import decode
-from CRUD.crud import *
-from CLASES.acceso import Acceso
+from CRUD.encripteitor import encode,decode
+from CRUD.crud import buscarClave, registroUsuarios
+from CONEX.conex import conex
 
+# Establece la conexión
+conection = conex()
 
 # El sistema considera sólo un tipo de usuario que lo manipulará, el cual corresponde al usuario encargado de biblioteca.
 # llaves para ingresar a menu
@@ -18,17 +20,24 @@ while True:
     op = input("Ingrese opción: ")
 
     if op == "1":
-        a = Acceso()# Instancia la clase Acceso para usar luego metodo registroUsuarios.
-        a.registroUsuarios()
+        usuario = input("Ingrese Usuario : ")
+        email = input("Ingrese Correo : ")
+        clave = input("Ingrese clave : ")
+        # Se llama directamente a la función de registro de usuarios
+        registroUsuarios(usuario, email, encode(clave), conection)
 
     elif op == "2":
         usuario = input("Ingrese usuario: ")
         clave = input("Ingrese su contraseña: ")
-        acceso = decode(clave, buscarClave(usuario, conection))
-        if acceso == True:  # Si el acceso es exitoso, deja entrar al menu.
-            m = Menu()
-        else:
-            print("Usuario y/o contraseña invalida")
+        try:
+            clave_bd = buscarClave(usuario, conection)
+            acceso = decode(clave, clave_bd)
+            if acceso:
+                m = Menu()
+            else:
+                print("Usuario y/o contraseña invalida")
+        except Exception as e:
+            print(f"Error al buscar la clave: {e}")
     elif op == "0":
         print("¡Hasta luego, saludos!")
         break
